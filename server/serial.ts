a4e7bedd2b9b6ed7f9d0d2160ed4b3a33873dce8
SamClercky <declerck.wim@gmail.com>
2020-01-19 13:06:55 +0100

    env fixed + audits

--- server/serial.ts
+++ server/serial.ts
@@ -1,8 +1,11 @@
 import EventManager from "./event_manager";
 
+const serial_port = process.env.serial_port as string;
+const serial_baudRate = parseInt(process.env.baudrate as string);
+
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
+const port = new SerialPort(serial_port, { baudRate: serial_baudRate });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
 // Read the port data
 port.on("open", () => {
