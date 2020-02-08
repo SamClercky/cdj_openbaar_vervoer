0c37fdddd3a39b19900b1dadfa6c6beab616c281
SamClercky <declerck.wim@gmail.com>
2020-02-08 15:12:50 +0100

    Server opkuis + package update + buffer test suite geschreven

--- server/event_manager.ts
+++ server/event_manager.ts
@@ -6,14 +6,33 @@ export interface EventData {
 
 export type EventListener = (msg: EventData) => void;
 
+/**
+ * Helper voor het rondsturen van berichten
+ */
 export default class EventManager {
     private static readonly _buffer: Buffer<any> = new Buffer(10);
     private static _listeners: EventListener[] = [];
 
+    /**
+     * Voeg een listener toe aan de globale berichten pijplijn
+     * @param listener 
+     */
     static addListener(listener: EventListener) {
         this._listeners.push(listener);
     }
 
+    /**
+     * Verwijder een listener toe aan de globale berichten pijplijn
+     * @param listener Callback die aangeroepen wordt bij ieder bericht
+     */
+    static removeListener(listener: EventListener) {
+        this._listeners = this._listeners.filter(item => item != listener);
+    }
+
+    /**
+     * Vuur een bericht af naar de buitenwereld
+     * @param msg Te sturen bericht
+     */
     static fireEvent(msg: EventData) {
         this._listeners.forEach(l => {
             l(msg);
--- server/serial.ts
+++ server/serial.ts
@@ -4,12 +4,14 @@ const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
-// Read the port data
+
+// Lezen van poort data
 port.on("open", () => {
-  console.log('serial port open');
+  console.log('Serial port open');
 });
+
 parser.on('data', (data: string) =>{
-  console.log('got word from arduino:', data);
+  console.log('[*] Got word from arduino:', data);
   EventManager.fireEvent({
     msg: data
   });
